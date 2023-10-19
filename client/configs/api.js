0c487d3dd87de2419a878a53395eb0f8083d23ad
Ayush-Bulbule <ayushbulbule24@gmail.com>
2023-10-19 11:15:50 +0530

    Deploy Changes

--- client/configs/api.js
+++ client/configs/api.js
@@ -1,8 +1,8 @@
 const apiConfig = {
-  // url: "https://api.submission.pulzion.co.in",
-  url: "http://localhost:8000",
-  accessKey: "AKIAXFJZDFAS7TYD63VX",
-  secretAccessKey: "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",
+  url: "https://api.submission.pulzion.co.in",
+  // url: "http://localhost:8000",
+  accessKey: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
   // srUi: "https://pasc-alumni-portal.s3.ap-south-1.amazonaws.com/",
 };
 
