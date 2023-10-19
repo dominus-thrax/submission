ceebad0f422347481d5727cc7af8560cc25ab4a0
Mustafa <100032108+TechyMT@users.noreply.github.com>
2023-10-19 23:49:37 +0530

    fix: changed sURI to actual sUri

--- client/configs/api.js
+++ client/configs/api.js
@@ -3,7 +3,7 @@ const apiConfig = {
   // url: "http://localhost:8000",
   accessKey: "AKIAXFJZDFAS7TYD63VX",
   secretAccessKey: "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",
-  // srUi: "https://pasc-alumni-portal.s3.ap-south-1.amazonaws.com/",
+  srUi: "https://pasc-alumni-portal.s3.ap-south-1.amazonaws.com/",
 };
 
 export default apiConfig;
