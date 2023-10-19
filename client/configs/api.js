c6a2c0eb2c13c214af5751da36e199d5b999157d
Mustafa <100032108+TechyMT@users.noreply.github.com>
2023-10-19 23:40:33 +0530

    submission platform url updated

--- client/configs/api.js
+++ client/configs/api.js
@@ -1,6 +1,6 @@
 const apiConfig = {
-  // url: "https://api.submission.pulzion.co.in",
-  url: "http://localhost:8000",
+  url: "https://api.submission.pulzion.co.in",
+  // url: "http://localhost:8000",
   accessKey: "AKIAXFJZDFAS7TYD63VX",
   secretAccessKey: "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",
   // srUi: "https://pasc-alumni-portal.s3.ap-south-1.amazonaws.com/",
