06cfeb413bae4e1eac8dcaa5b130120427e76d1c
PranavJaju <pranavjaju45@gmail.com>
2023-10-21 21:57:46 +0530

    key change

--- client/configs/api.js
+++ client/configs/api.js
@@ -1,8 +1,8 @@
 const apiConfig = {
   url: "https://api.submission.pulzion.co.in",
   // url: "http://localhost:8000",
-  accessKey: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY,
-  secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_KEY,
+  accessKey:"AKIAXFJZDFASXG3RJ73L",
+  secretAccessKey: "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",
   // srUi: "https://pasc-alumni-portal.s3.ap-south-1.amazonaws.com/",
 };
 
