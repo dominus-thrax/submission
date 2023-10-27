b99635e789b237789acc2bdc37f0bbfa3a2c96bb
PranavJaju <pranavjaju45@gmail.com>
2023-10-27 13:58:39 +0530

    Connect EMS

--- client/configs/api.js
+++ client/configs/api.js
@@ -1,6 +1,6 @@
 const apiConfig = {
-  // url: "https://api.submission.pulzion.co.in",
-  url: "http://localhost:8000",
+  url: "https://api.submission.pulzion.co.in",
+  // url: "http://localhost:8000",
   accessKey:"AKIAXFJZDFASXG3RJ73L",
   secretAccessKey: "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",
   // srUi: "https://pasc-alumni-portal.s3.ap-south-1.amazonaws.com/",
